bc5ee8e0af176ef8722b2b96a6988ac0a49910f8
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-07 19:47:12 +0200

    Improved console logs

--- src/appveyor.ts
+++ src/appveyor.ts
@@ -5,17 +5,17 @@ export class AppVeyor
 {
     public static async getJobLog(jobId: string): Promise<string>
     {
-        console.log("Querying for AppVeyor job log...");
+        console.log(`Querying AppVeyor for log of job '${jobId}'...`);
         const response = await axios.get(`https://ci.appveyor.com/api/buildjobs/${jobId}/log`);
         if (response.status === 200)
         {
-            console.log("AppVeyor job log obtained successfully");
+            console.log("Job log obtained successfully from AppVeyor.");
             console.log("");
             return response.data;
         }
         else
         {
-            throw Error("Error while querying AppVeyor job log: " + JSON.stringify(response));
+            throw Error("Error while querying AppVeyor for job log: " + JSON.stringify(response));
         }
     }
 }
