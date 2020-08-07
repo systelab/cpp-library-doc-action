25dbd1e0b9628f8c167e1e7d67ca031f4f7ee52d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-07 19:47:55 +0200

    Added log reporter (agnostic to CI)

---
+++ src/log-reporter.ts
@@ -0,0 +1,45 @@
+import * as core from "@actions/core";
+import * as fs from "fs";
+
+import { AppVeyor } from "./appveyor";
+
+
+export class LogReporter
+{
+    public static async buildLogReportFile(): Promise<string>
+    {
+        const logContent: string = await this.getJobLog();
+        const reportFilename =  this.getLogReportFilepath();
+        fs.writeFileSync(reportFilename, logContent);
+
+        return reportFilename;
+    }
+
+    private static async getJobLog(): Promise<string>
+    {
+        const ciSystem = core.getInput("ci-system");
+        const jobId = core.getInput("job-id");
+
+        if (ciSystem === "AppVeyor")
+        {
+            return AppVeyor.getJobLog(jobId);
+        }
+        else if (ciSystem === "Travis")
+        {
+            throw Error("Travis log retrieval not implemented yet");
+        }
+        else
+        {
+            throw Error("Unsupported continuous integration system");
+        }
+    }
+
+    private static getLogReportFilepath(): string
+    {
+        const libraryName = core.getInput("library-name");
+        const tagName = core.getInput("tag-name");
+        const configurationName = core.getInput("configuration-name");
+
+        return `${libraryName}-${tagName}-${configurationName}-BuildLog.log`;
+    }
+}
