93ddd52e99d9b6cdc2996c58c93af604981d8031
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-19 12:40:33 +0200

    Added method to download an AppVeyor artifact

--- src/appveyor.ts
+++ src/appveyor.ts
@@ -18,4 +18,20 @@ export class AppVeyor
             throw Error("Error while querying AppVeyor for job log: " + JSON.stringify(response));
         }
     }
+
+    public static async getJobArtifactContent(jobId: string, filename: string): Promise<string>
+    {
+        console.log(`Querying AppVeyor for artifact '${filename}' of job '${jobId}'...`);
+        const response = await axios.get(`https://ci.appveyor.com/api/buildjobs/${jobId}/artifacts/${filename}`);
+        if (response.status === 200)
+        {
+            console.log("Artifact obtained successfully from AppVeyor.");
+            console.log("");
+            return response.data;
+        }
+        else
+        {
+            throw Error("Error while querying AppVeyor for job artifact: " + JSON.stringify(response));
+        }
+    }
 }
