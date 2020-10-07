31e35ff91ec33824d785d9481e6d882cbac1558d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-07 19:45:51 +0200

    Clone given repo in workspace (first step to gather changelog)

--- src/reporters/changelog.reporter.ts
+++ src/reporters/changelog.reporter.ts
@@ -1,11 +1,18 @@
-import { ReleaseBuild } from "@model";
+import simpleGit, { SimpleGit } from "simple-git";
+
+import { ReleaseBuild, Repository } from "@model";
+import { FilesystemUtility } from "@utils";
 import { PDFReporter } from "./pdf.reporter";
 
 
 export class ChangelogReporter
 {
+    static gitClient: SimpleGit = simpleGit();
+
     public static async generateChangelogReportFile(build: ReleaseBuild): Promise<string>
     {
+        await this.cloneRepository(build.repository);
+
         const reportTitle = this.getReportTitle(build);
         const reportFilename =  this.getReportFilepath(build);
         const htmlReportContent = await this.getHTMLReportContent(build);
@@ -13,6 +20,23 @@ export class ChangelogReporter
         await PDFReporter.generate(reportTitle, htmlReportContent, reportFilename);
 
         return reportFilename;
+    }
+
+    private static async cloneRepository(repository: Repository): Promise<void>
+    {
+        const repoLocalPath = this.getRepositoryLocalPath(repository);
+        console.log(`Delete '${repoLocalPath}' folder...`);
+        FilesystemUtility.deleteFolder(repoLocalPath);
+        console.log("Folder deleted successfully.");
+
+        console.log(`Cloning '${repository.url}' into '${repoLocalPath}' repository...`);
+        const cloneResult = await this.gitClient.clone(repository.url, repoLocalPath);
+        console.log("Clone result", cloneResult);
+    }
+
+    private static getRepositoryLocalPath(repository: Repository): string
+    {
+        return `./workspace/${repository.slug}`;
     }
 
     private static getReportTitle(build: ReleaseBuild): string
