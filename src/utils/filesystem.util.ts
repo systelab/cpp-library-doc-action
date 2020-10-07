3fbf7f3b4b153b8d5f2efafe90ae88f497dab8ec
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-07 19:43:57 +0200

    Added new utility to easily work with filesystem

---
+++ src/utils/filesystem.util.ts
@@ -0,0 +1,85 @@
+import * as fs from "fs";
+import * as path from "path";
+
+
+export class FilesystemUtility
+{
+    public static readFile(filePath: string, encoding = null): string
+    {
+        return fs.readFileSync(filePath, encoding).toString();
+    }
+
+    public static readFileLines(filepath: string): string[]
+    {
+        try
+        {
+            const data: string = this.readFile(filepath, "UTF-8");
+            return data.split(/\r?\n/);
+        }
+        catch (Exception)
+        {
+            return [];
+        }
+    }
+
+    public static deleteFolder(folderPath: string): void
+    {
+        if (this.exists(folderPath))
+        {
+            this.deleteFolderContents(folderPath);
+            this.deleteEmptyFolder(folderPath, 3);
+        }
+    }
+
+    public static deleteEmptyFolder(folderPath: string, maxRetries = 1)
+    {
+        for (let i = 1; i < maxRetries; i++)
+        {
+            try
+            {
+                fs.rmdirSync(folderPath);
+                return;
+            }
+            catch (e)
+            {
+            }
+        }
+
+        fs.rmdirSync(folderPath);
+    }
+
+    public static deleteFolderContents(folderPath: string): void
+    {
+        if (this.exists(folderPath))
+        {
+            fs.readdirSync(folderPath).forEach((folderEntry, index) =>
+            {
+                const filepath = path.join(folderPath, folderEntry);
+                if (fs.lstatSync(filepath).isDirectory())
+                {
+                    this.deleteFolderContents(filepath);
+                    fs.rmdirSync(filepath);
+                }
+                else
+                {
+                    this.deleteFile(filepath);
+                }
+            });
+        }
+    }
+
+    public static deleteFile(filepath: string): void
+    {
+        fs.unlinkSync(filepath);
+    }
+
+    public static exists(filesystemPath: string): boolean
+    {
+        return fs.existsSync(filesystemPath);
+    }
+
+    public static getAbsolutePath(relativePath: string): string
+    {
+        return path.resolve(relativePath);
+    }
+}
