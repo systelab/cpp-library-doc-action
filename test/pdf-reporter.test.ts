5e8ddfe849c309ae342c1bcb986cce316d5405c8
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-08 12:22:38 +0200

    Refactored test to faciliate definition of new cases

--- test/pdf-reporter.test.ts
+++ test/pdf-reporter.test.ts
@@ -1,25 +1,37 @@
-import * as fs from "fs";
+import { PDFDocument } from "@model";
 import { PDFReporter } from "@reporters";
-
+import { DateUtility, FilesystemUtility } from "@utils";
 
 
 describe("PDFReporter", () =>
 {
-    it("Generate a PDF with content of 'test/resources/compilation.log' file", async () =>
+    let reportContent: string;
+
+    before(async () =>
     {
         let logContentHTML = "";
-        const logContent = fs.readFileSync("test/resources/compilation.log").toString();
-        const logContentLines = logContent.split(/\r\n|\n|\r/);
+        const logContentLines = FilesystemUtility.readFileLines("test/resources/compilation.log");
         for (const logLine of logContentLines)
         {
             logContentHTML += `<div class="log-line">${logLine}</div>`;
         }
 
-        const reportContent = `<h1>1 Introduction</h1>
-                            <p class="last">Trace API version 1.0.4 was built on August 8th, 2020 following steps described in build procedure.</p>
-                            <h1>2 Report</h1>
-                            <div>${logContentHTML}</div>`;
-
-        await PDFReporter.generate("Trace API compilation memo for version 1.0.4", reportContent, "report/test-report.pdf");
+        reportContent = `<h1>1 Introduction</h1>` +
+                        `<p class="last">This is an example of introduction section for PDF reports</p>` +
+                        `<h1>2 Report</h1>` +
+                        `<div>${logContentHTML}</div>`;
     });
+
+    it("Generate a PDF report with the minimum fields on header", async () =>
+    {
+        const pdfDocument: PDFDocument = {
+            filepath: "report/pdf-reporter-test-minimum.pdf",
+            title: "Test of PDF with minimum fields on header",
+            date: DateUtility.getCurrrentDateForHeader(),
+            content: reportContent
+        };
+
+        await PDFReporter.generate(pdfDocument);
+    });
+
 });
