afd37f44f735099c0e0925911100eda9e299f3bd
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-08 12:45:53 +0200

    Added test for build log reporter

---
+++ test/buildlog-reporter.test.ts
@@ -0,0 +1,26 @@
+import { ContinuousIntegrationSystem, ReleaseBuild, Repository, RepositoryType } from "@model";
+import { BuildlogReporter } from "@reporters";
+
+
+describe("BuildlogReporter", () =>
+{
+    it("Generate a PDF with the change log report of 'https://github.com/systelab/cpp-json-settings' repository " +
+       "for version 'v1.3.0' since repository creation", async () =>
+    {
+        const repository: Repository = {
+            type: RepositoryType.GitHub,
+            owner: "systelab",
+            slug: "cpp-trace-api",
+            name: "C++ Trace API Library",
+            url: "https://github.com/systelab/cpp-trace-api.git"
+        };
+        const build: ReleaseBuild = {
+            repository,
+            tag: "v1.0.4",
+            configuration: "Visual Studio 2017 Win32 Release",
+            ciSystem: ContinuousIntegrationSystem.AppVeyor,
+            jobId: "04qt9dlcisd8mqqc"
+        };
+        await BuildlogReporter.generateBuildlogReportFile(build);
+    });
+});
