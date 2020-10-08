379a7c3caec2d8d57972133ba3e5f04b520e39a1
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-08 10:30:33 +0200

    Added tests for different changelog ranges: all repo and since a tag

--- test/changelog-reporter.test.ts
+++ test/changelog-reporter.test.ts
@@ -4,7 +4,22 @@ import { ChangelogReporter } from "@reporters";
 
 describe("ChangelogReporter", () =>
 {
-    it("Generate a PDF with the change log report of 'https://github.com/systelab/cpp-json-settings' repository for tag 'v1.3.4'", async () =>
+    it("Generate a PDF with the change log report of 'https://github.com/systelab/cpp-json-settings' repository " +
+       "for version 'v1.3.0' since repository creation", async () =>
+    {
+        const repository: Repository = {
+            type: RepositoryType.GitHub,
+            owner: "systelab",
+            slug: "cpp-json-settings",
+            name: "C++ JSON Settings Library",
+            url: "https://github.com/systelab/cpp-json-settings.git"
+        };
+        const tag = "v1.3.0";
+        await ChangelogReporter.generateChangelogReportFile(repository, tag);
+    });
+
+    it("Generate a PDF with the change log report of 'https://github.com/systelab/cpp-json-settings' repository " +
+       "for version 'v1.3.4' since tag 'v1.3.0'", async () =>
     {
         const repository: Repository = {
             type: RepositoryType.GitHub,
@@ -14,19 +29,8 @@ describe("ChangelogReporter", () =>
             url: "https://github.com/systelab/cpp-json-settings.git"
         };
         const tag = "v1.3.4";
-        await ChangelogReporter.generateChangelogReportFile(repository, tag);
+        const baseTag = "v1.3.0";
+        await ChangelogReporter.generateChangelogReportFile(repository, tag, baseTag);
     });
 
-    it("Generate a PDF with the change log report of 'https://bitbucket.org/Systelab/cpp-anti-debug' repository for tag 'v1.0.3'", async () =>
-    {
-        const repository: Repository = {
-            type: RepositoryType.Bitbucket,
-            owner: "Systelab",
-            slug: "cpp-anti-debug",
-            name: "C++ AntiDebug Library",
-            url: "https://bitbucket.org/Systelab/cpp-anti-debug.git"
-        };
-        const tag = "v1.0.3";
-        await ChangelogReporter.generateChangelogReportFile(repository, tag);
-    });
 });
