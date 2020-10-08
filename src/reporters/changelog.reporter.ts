05255f3d396b85db4a80cc3479fbf4d87710c6b5
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-08 10:12:11 +0200

    Updated reporter to generate a section for each tag

--- src/reporters/changelog.reporter.ts
+++ src/reporters/changelog.reporter.ts
@@ -1,4 +1,5 @@
 import simpleGit, { SimpleGit } from "simple-git";
+import gitlog, { GitlogOptions } from "gitlog";
 
 import { Repository } from "@model";
 import { FilesystemUtility } from "@utils";
@@ -7,15 +8,15 @@ import { PDFReporter } from "./pdf.reporter";
 
 export class ChangelogReporter
 {
-    static gitClient: SimpleGit = simpleGit();
+    static gitClient: SimpleGit;
 
-    public static async generateChangelogReportFile(repository: Repository, tag: string): Promise<string>
+    public static async generateChangelogReportFile(repository: Repository, tag: string, baseTag?: string): Promise<string>
     {
         await this.cloneRepository(repository);
 
         const reportTitle = this.getReportTitle(repository, tag);
         const reportFilename =  this.getReportFilepath(repository, tag);
-        const htmlReportContent = await this.getHTMLReportContent(repository, tag);
+        const htmlReportContent = await this.getHTMLReportContent(repository, tag, baseTag);
 
         await PDFReporter.generate(reportTitle, htmlReportContent, reportFilename);
 
@@ -25,18 +26,27 @@ export class ChangelogReporter
     private static async cloneRepository(repository: Repository): Promise<void>
     {
         const repoLocalPath = this.getRepositoryLocalPath(repository);
-        console.log(`Delete '${repoLocalPath}' folder...`);
+        console.log(`Deleting '${repoLocalPath}' folder contents...`);
         FilesystemUtility.deleteFolder(repoLocalPath);
-        console.log("Folder deleted successfully.");
+        FilesystemUtility.createFolder(repoLocalPath);
+        console.log("Contents of folder deleted successfully.");
+        console.log("");
+
+        this.gitClient = simpleGit({
+            baseDir: `${process.cwd()}/${repoLocalPath}`,
+            binary: "git",
+            maxConcurrentProcesses: 6,
+        });
 
         console.log(`Cloning '${repository.url}' into '${repoLocalPath}' repository...`);
-        const cloneResult = await this.gitClient.clone(repository.url, repoLocalPath);
-        console.log("Clone result", cloneResult);
+        await this.gitClient.clone(repository.url, ".");
+        console.log("Repository cloned successfully.");
+        console.log("");
     }
 
     private static getRepositoryLocalPath(repository: Repository): string
     {
-        return `./workspace/${repository.slug}`;
+        return `workspace/${repository.slug}`;
     }
 
     private static getReportTitle(repository: Repository, tag: string): string
@@ -49,14 +59,89 @@ export class ChangelogReporter
         return `report/${repository.slug}-${tag}-Changelog.pdf`;
     }
 
-    private static async getHTMLReportContent(repository: Repository, tag: string): Promise<string>
+    private static async getHTMLReportContent(repository: Repository, tag: string, baseTag: string): Promise<string>
     {
-        // const options: GitlogOptions = {
-        //     repo: ActionInput.getRepositoryName(),
-        //     fields: ["subject", "authorName", "authorDate"] as const,
-        // };
-        // const entries = gitlog(options);
+        const changelogBase = baseTag ? `tag for version ${baseTag.substr(1)}` : "repository creation";
+        let content = "<h1>1 Introduction</h1>" +
+                      `<p class="last">This report contains all commits for version ${tag.substr(1)} of ${repository.name} since ${changelogBase}.</p>`;
 
-        return "TODO";
+        let sectionId = 2;
+        const intermediateTags: string[] = await this.getIntermediateTags(tag, baseTag);
+        for (let tagIndex = 0; tagIndex < intermediateTags.length; tagIndex++)
+        {
+            const currentTag =  intermediateTags[tagIndex];
+            const previousTag = (tagIndex < intermediateTags.length - 1) ? intermediateTags[tagIndex + 1] : baseTag;
+            content += await this.getHTMLReportTagSection(sectionId, repository, currentTag, previousTag);
+            sectionId++;
+        }
+
+        return content;
+    }
+
+    private static async getIntermediateTags(tag: string, baseTag: string): Promise<string[]>
+    {
+        const intermediateTags: string[] = [];
+
+        let insideTagsInterval = !baseTag;
+        const allTags = await this.gitClient.tags();
+        for (let tagIndex = 0; tagIndex < allTags["all"].length; tagIndex++)
+        {
+            const currentTag = allTags["all"][tagIndex];
+            if (insideTagsInterval)
+            {
+                intermediateTags.push(currentTag);
+            }
+
+            if (currentTag === baseTag)
+            {
+                insideTagsInterval = true;
+            }
+            else if (currentTag === tag)
+            {
+                insideTagsInterval = false;
+            }
+        }
+
+        return intermediateTags.reverse();
+    }
+
+    private static async getHTMLReportTagSection(sectionId: number, repository: Repository, tag: string, baseTag: string): Promise<string>
+    {
+        let content = `<h1>${sectionId} Changes for ${tag}</h1>`;
+
+        const logOptions = {
+            repo: this.getRepositoryLocalPath(repository),
+            fields: ["authorName", "authorDate", "hash", "rawBody"] as const,
+            branch: await this.getRevisionRange(tag, baseTag),
+            number: 10000000
+        };
+
+        const commits = await gitlog(logOptions);
+        for (const commit of commits)
+        {
+            content += `<div class="commit-item">`;
+            content += `<b>${commit.authorDate} - ${commit.authorName}</b><br>`;
+            content += `${commit.hash}: ${this.getSanitizedHTML(commit.rawBody)}<br><br>`;
+            content += `</diV>`;
+        }
+
+        return content;
+    }
+
+    private static async getRevisionRange(tag: string, previousTag: string): Promise<string>
+    {
+        if (previousTag && (previousTag.length > 0))
+        {
+            return `${previousTag}...${tag}`;
+        }
+        else
+        {
+            return tag;
+        }
+    }
+
+    private static getSanitizedHTML(data: string): string
+    {
+        return data.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\n/g, "<br>");
     }
 }
