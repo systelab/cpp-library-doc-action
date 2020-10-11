259a83272e65bf71002e9c8cca3be4fcddab0aa6
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-11 13:28:25 +0200

    Added interface for Test result parser utility.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./date.util";
 export * from "./filesystem.util";
+export * from "./test-result.util";
---
+++ src/utils/test-result.util.ts
@@ -0,0 +1,10 @@
+import { TestSuiteResult } from "@model";
+
+
+export class TestResultUtility
+{
+    public static parseTestSuiteResult(xmlFilepath: string): TestSuiteResult
+    {
+        return null;
+    }
+}
