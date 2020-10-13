c38a5166db14f526380da593c502ab4472abec61
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-10-13 09:05:42 +0200

    Integarated use of DOM parser

--- src/utils/test-result.util.ts
+++ src/utils/test-result.util.ts
@@ -1,10 +1,28 @@
+import * as DomParser from "dom-parser";
+
 import { TestSuiteResult } from "@model";
+import { FilesystemUtility } from "@utils";
 
 
 export class TestResultUtility
 {
     public static parseTestSuiteResult(xmlFilepath: string): TestSuiteResult
     {
-        return null;
+        try
+        {
+            const fileContent = FilesystemUtility.readFile(xmlFilepath);
+            const parser = new DomParser();
+            const xmlDoc = parser.parseFromString(fileContent);
+
+            const testSuites = xmlDoc.getElementsByTagName("testsuites")[0];
+
+            return { testCases: [] };
+        }
+        catch (e)
+        {
+            return null;
+        }
     }
+
+
 }
