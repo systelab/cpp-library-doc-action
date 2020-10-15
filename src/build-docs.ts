38e7ba3cf19a169e9aed6ec9088202e2a4dcff5f
Joaquim Vila Bou <jvila2@werfen.com>
2020-10-15 11:37:52 +0200

    Prepared script to include version on changelog header

--- src/build-docs.ts
+++ src/build-docs.ts
@@ -14,9 +14,10 @@ describe("Generate Release Documents", () =>
             const repository: Repository = configuration.build.repository;
             const tag: string = configuration.build.tag;
             const status: string = configuration.changeLogReport.status;
+            const version: string = configuration.changeLogReport.version;
             const code: string = configuration.changeLogReport.code;
             const filepath: string = WorkspaceUtility.buildPath(configuration.changeLogReport.filepath);
-            await ChangelogReporter.generateReport({repository, tag, status, code, filepath});
+            await ChangelogReporter.generateReport({repository, tag, status, version, code, filepath});
         });
     }
 
